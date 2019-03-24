32d547c8785a9e9a7ca3675fe7b814c494cb8cf9
Maaryamh <48865826+Maaryamh@users.noreply.github.com>
2019-03-24 13:51:49 +0400

    Delete backpack.config.js

--- backpack.config.js
+++
@@ -1,8 +0,0 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-module.exports = {
-  webpack: (config, options) => {
-    config.entry.main = ['./server/index.js'];
-
-    return config;
-  },
-};
